0b80509aed7474de7a36a62c355d0d2bf9d02215
Olivier Revollat <revollat@gmail.com>
2015-03-18 17:05:04 +0100

    éviter vulnérabilité XSS

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-// framework/index.php
+// des libs comme twig permettent d'échapper automatiquement le text et simplifie donc ce traitement 
 
 $input = isset($_GET['name']) ? $_GET['name'] : 'World';
 
-printf('Hello %s', $input);
+header('Content-Type: text/html; charset=utf-8');
+
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
