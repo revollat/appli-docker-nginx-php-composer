88c7ec5e0e964912bf8e84903a1092478e7c7bbb
Olivier Revollat <revollat@gmail.com>
2015-03-20 15:17:15 +0100

    mise en commun du code (refactoring) dans init.php

--- bye.php
+++ bye.php
@@ -2,12 +2,7 @@
 
 // framework/bye.php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
-
-$response = new Response('Goodbye!');
+$response->setContent('Goodbye!');
 $response->send();
