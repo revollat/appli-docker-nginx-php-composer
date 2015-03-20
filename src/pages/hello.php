6f63dba4f6ee3f7024f4a0bc5443f85e93b0a2bc
Olivier Revollat <revollat@gmail.com>
2015-03-20 16:16:25 +0100

    generation route dynamique dans template

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,9 @@
 <!-- example.com/src/pages/hello.php -->
+<?php
+use Symfony\Component\Routing;
+$generator = new Routing\Generator\UrlGenerator($routes, $context);
+?>
 
 Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+<br><br>
+<a href="<?php echo $generator->generate('hello', array('name' => 'LP-IMASH'));?>">Hello lpimash</a>
