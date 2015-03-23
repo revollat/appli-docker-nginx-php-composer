a8a2abe3aa553c42b427a51e4ba26b90307bfeb3
Olivier Revollat <revollat@gmail.com>
2015-03-23 12:02:38 +0100

    Exemple: définition d'un controleur 'inline' personalisé

--- src/app.php
+++ src/app.php
@@ -7,7 +7,18 @@ use Symfony\Component\Routing;
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
     'name' => 'World',
-    '_controller' => 'render_template'
+    '_controller' => function ($request) {
+            // $foo sera dispo dans le template
+            $request->attributes->set('foo', 'bar');
+
+            // on appelle le controleur par défaut
+            $response = render_template($request);
+
+            // modification des header de la réponse
+            $response->headers->set('Content-Type', 'text/plain');
+
+            return $response;
+        }
 )));
 $routes->add('bye', new Routing\Route('/bye', array(
     '_controller' => 'render_template'
