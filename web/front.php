633e55fd9b5f31711439f01bb6847f3355524149
Olivier Revollat <revollat@gmail.com>
2015-03-23 11:57:49 +0100

    utilisation de l'attibut _controller dans la définition de la route pour controleur

--- web/front.php
+++ web/front.php
@@ -26,11 +26,12 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func('render_template', $request);
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
     $response = new Response('An error occurred', 500);
 }
+
 
 $response->send();
