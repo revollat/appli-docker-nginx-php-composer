dc06187626805ab0848ccd51dd38523468e5ba18
Olivier Revollat <revollat@gmail.com>
2015-03-23 11:49:49 +0100

    Ajout d'une fonction controleur

--- web/front.php
+++ web/front.php
@@ -8,24 +8,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context); /* pour autres exemples cf. https://github.com/fabpot/Create-Your-Framework/blob/master/book/part04.rst
-																print_r($matcher->match('/hello'));
-																array (
-																  'name' => 'World',
-																  '_route' => 'hello',
-																);*/
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route); // Route names are used for template names;
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
